e4dc2530994b58f341837a6b6aa45a844a2ba10f
clara <clara@clara.com>
2018-02-14 11:17:13 -0600

    remove flash messages from this version

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var flash = require('express-flash');
-var session = require('express-session');
 var mongoose = require('mongoose');
 
 // Read the mLab connection URL
@@ -32,9 +30,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(session({secret: 'top secret', resave: false, saveUninitialized: false}));
-app.use(flash());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
